b59f21d7f1946b7a2c8c15d16670a181a5edb4a5
JessyMakkry <jessy.makkry@bk.ru>
2024-03-23 13:37:18 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,13 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //Задание 3
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
     }
 }
