89f076b2ae6c7f58d648bbdde38ea17ea2e94a48
JessyMakkry <jessy.makkry@bk.ru>
2024-03-23 13:38:48 +0300

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,13 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //Задание 4
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
 
     }
 }
