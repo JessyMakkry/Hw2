67976e33cdd9b51948fd1de4869416391740377d
JessyMakkry <jessy.makkry@bk.ru>
2024-03-23 13:35:05 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //Задание 2
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
     }
 }
